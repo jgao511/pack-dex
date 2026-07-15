083f8a2791bac6159a12f25388fcc454f8309545
Jonathan <jonjon511@gmail.com>
2026-07-15 16:59:19 -0400

    Build deployed Scanner tab assets

---
+++ dist/mobile-app/assets/web-Doeg_sPu.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CA_OdS84.js";var t=class extends e{detectText(e){return Promise.reject(`Web Plugin Not implemented`)}};export{t as CapacitorPluginMlKitTextRecognitionWeb};
