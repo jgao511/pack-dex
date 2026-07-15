c9dfa0f99719203d04e82ac4cbb2cafcb9ffc5eb
Jonathan <jonjon511@gmail.com>
2026-07-15 19:24:28 -0400

    Simplify scanner result and improve scan tips

---
+++ dist/mobile-app/assets/web-BO-9N0yw.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-Df-MnWfy.js";var t=class extends e{detectText(e){return Promise.reject(`Web Plugin Not implemented`)}};export{t as CapacitorPluginMlKitTextRecognitionWeb};
